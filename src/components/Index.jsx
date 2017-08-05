4bf7cc57593c28a4eb924fde1edae90a9a3d1290
_knaut <info@knaut.net>
2017-08-05 14:49:47 -0700

    preparing to integrate babel to webpack

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -3,6 +3,19 @@ import React from 'react';
 
 class Test extends React.Component {
   render() {
+
+    var test = {
+      a: 1,
+      b: 2,
+      c: 3
+    };
+
+    var newTest = {
+      ...test
+    };
+
+    console.log(newTest)
+
     return(
       <div>This is a good test</div>
     )
