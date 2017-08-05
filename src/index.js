68cbef25703c87cca31a7f7872228abe0deb9dde
_knaut <info@knaut.net>
2017-08-04 23:45:24 -0700

    adding plain css automatically with style loaders

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
   var element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpack', 'and stuff in the world'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack', 'and in the world'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
